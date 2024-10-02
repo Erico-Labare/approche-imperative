976b4145ed033af860243e0e88d943bd931aeb82
Erico Labare <erico.labare@outlook.com>
2024-10-02 14:57:00 +0200

    update TP2

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,20 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte byte1 = 1;
+        short short1 = 2;
+        int int1 = 3;
+        long long1 = 4;
+        float float1 = 5;
+        double double1 = 6;
+        char char1 = '7';
+        boolean boolean1 = true;
+        String String1 = "9";
+
+        System.out.println(float1);
+
+        String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
+        System.out.println(randomString);
+    }
+}
